78da86bc49cb81ff2c8c083ad088f44e1aa23304
Bastien Couder <bco@sga-industries.com>
2024-11-29 09:36:37 +0100

    middleware

---
+++ apps/docs/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+
+export function middleware(request: NextRequest): NextResponse {
+
+  if (request.nextUrl.pathname === '/components') {
+   
+    const url = request.nextUrl.clone();
+    url.pathname = '/components/react/accordion';
+
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/components',
+};
